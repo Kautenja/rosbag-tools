9240abebf84fd1db73dab4839ab596bb5510d9ea
Kautenja <kautencreations@gmail.com>
2018-12-01 23:56:49 -0600

    update play depth to new style

--- play_depth.py
+++ play_depth.py
@@ -1,19 +1,17 @@
 """A script to play raw ZED footage."""
 import argparse
 from rosbag import Bag
-from src.ros_utils import get_camera_dimensions
 from src.ros_utils import get_camera_image
 from src.ros_utils import get_depth_image
 from src.window import Window
 
 
-def play_depth(bag_file: Bag, camera_info: str, camera: str, depth: str) -> None:
+def play_depth(bag_file: Bag, camera: str, depth: str) -> None:
     """
     Play the ZED data in a bag file.
 
     Args:
         bag_file: the bag file to play
-        camera_info: the topic to use to read metadata about the camera
         camera: the topic to use to read compressed or raw camera data
         depth: the topic to use to read 32-bit floating point depth measures
 
@@ -21,20 +19,34 @@ def play_depth(bag_file: Bag, camera_info: str, camera: str, depth: str) -> None
         None
 
     """
-    # extract the camera dimensions from the bag
-    dims = get_camera_dimensions(bag, camera_info)
     # open windows to stream the camera and depth image data to
-    camera_window = Window('{} ({})'.format(bag_file.filename, camera), *dims)
-    depth_window = Window('{} ({})'.format(bag_file.filename, depth), *dims)
+    camera_window = None
+    depth_window = None
     # iterate over the messages
     for topic, msg, _ in bag.read_messages(topics=[camera, depth]):
         # if topic is depth, unwrap and send to the depth window
         if topic == depth:
-            img = get_depth_image(msg.data, dims)
+            # if the depth window is not setup yet, open it
+            if depth_window is None:
+                # create a title for the window
+                title = '{} ({})'.format(bag_file.filename, depth)
+                # initialize the window
+                depth_window = Window(title, msg.height, msg.width)
+            # get a depth image from the data and dimensions
+            img = get_depth_image(msg.data, depth_window.shape)
+            # show the image on the depth window
             depth_window.show(img)
         # if topic is camera, unwrap and send to the camera window
         elif topic == camera:
-            img = get_camera_image(msg.data, dims)
+            # if the camera window is not setup yet, open it
+            if camera_window is None:
+                # create a title for the window
+                title = '{} ({})'.format(bag_file.filename, camera)
+                # initialize the window
+                camera_window = Window(title, msg.height, msg.width)
+            # get an image from the data and dimensions
+            img = get_camera_image(msg.data, camera_window.shape)
+            # show the image on the camera window
             camera_window.show(img)
     # shut down the viewer windows
     camera_window.close()
@@ -53,19 +65,12 @@ if __name__ == '__main__':
         help='The bag file containing the ZED data to play.',
         required=True,
     )
-    # add an argument for selecting the camera info topic
-    PARSER.add_argument('--camera_info', '-C',
-        type=str,
-        help='The topic to get information about the camera from.',
-        required=False,
-        default='/zed/left/camera_info_raw',
-    )
     # add an argument for selecting the camera data topic
     PARSER.add_argument('--camera', '-c',
         type=str,
         help='The topic to use to read camera data.',
         required=False,
-        default='/zed/left/image_rect_color/compressed',
+        default='/zed/left/image_rect_color',
     )
     # add an argument for selecting the depth data topic
     PARSER.add_argument('--depth', '-d',
@@ -79,7 +84,7 @@ if __name__ == '__main__':
     # play the bag given in the arguments
     try:
         with Bag(ARGS.bag_file) as bag:
-            play_depth(bag, ARGS.camera_info, ARGS.camera, ARGS.depth)
+            play_depth(bag, ARGS.camera, ARGS.depth)
     except KeyboardInterrupt:
         pass
 
